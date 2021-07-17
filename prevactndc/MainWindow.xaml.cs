05c3787eedb9248de6a0d73876d6116623b4d964
Kevin BAMOUNI <kevincedric.y@gmail.com>
2021-07-17 21:20:17 +0200

    Mainwindows : Disposition des grid!!!

--- prevactndc/MainWindow.xaml.cs
+++ prevactndc/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace prevactndc
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
