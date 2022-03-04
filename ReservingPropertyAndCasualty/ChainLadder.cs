285cc5b299bccae43d8aed19d2920e15042a5af7
Kevin BAMOUNI <kevincedric.y@gmail.com>
2022-03-04 22:04:11 +0100

    6 Ajout des tests XUNIT

--- ReservingPropertyAndCasualty/ChainLadder.cs
+++ ReservingPropertyAndCasualty/ChainLadder.cs
@@ -8,61 +8,70 @@ using NumSharp;
 
 namespace ReservingPropertyAndCasualty
 {
-    internal class ChainLadder
-    {   
+    public static class ChainLadder
+    {
         // Les tableaux sont indexés sur zéro : un tableau avec n éléments est indexée de 0 à n-1
-        decimal[,] CumulativeSumTriangle(decimal[,] triangleArray)
+        public static decimal[,] CumulativeSumTriangle(decimal[,] triangleArray)
         {
             decimal[,] sumTriangleArray = triangleArray;
             int nRows = triangleArray.GetLength(0);
             int nColumns = triangleArray.GetLength(1);
-            for(int i=0; i<nRows-1; nRows++){
-                for (int j = 1; j <= nColumns-1; nRows++){
+            for(int i=0; i < nRows; i++){
+                for (int j = 1; j < nColumns; j++){
                     sumTriangleArray[i,j]= sumTriangleArray[i,j]+ sumTriangleArray[i,j-1];
                 }
             }
             return sumTriangleArray;
         }
 
-        decimal[] GetColumnN(decimal[,] triangleArray, int n)
-        {
-            int nRows = triangleArray.GetLength(0);
-            decimal[] array = new decimal[nRows];
-            for (int i = 0; i < nRows - 1; nRows++)
-            {
-                array[i] = triangleArray[i,n];
-            }
-            return array;
-        }
-
-        decimal[] GetRowN(decimal[,] triangleArray, int n)
+        public static decimal[] GetColumnN(decimal[,] triangleArray, int n)
         {
             int nColumns = triangleArray.GetLength(1);
-            decimal[] array = new decimal[nColumns];
-            for (int i = 0; i < nColumns - 1; nColumns++)
+            if (n < nColumns)
             {
-                array[i] = triangleArray[n, i];
+                decimal[] array = new decimal[nColumns];
+                for (int i = 0; i < nColumns; i++)
+                {
+                    array[i] = triangleArray[i, n];
+                }
+                return array;
             }
-            return array;
+            else { throw new IndexOutOfRangeException(); }
         }
 
-        decimal[] TriangleMainDiagonal(decimal[,] triangleArray)
+        public static decimal[] GetRowN(decimal[,] triangleArray, int n)
+        {
+            
+            int nRows = triangleArray.GetLength(0);
+            if (n < nRows)
+            {
+                decimal[] array = new decimal[nRows];
+                for (int i = 0; i < nRows; i++)
+                {
+                    array[i] = triangleArray[n, i];
+                }
+                return array;
+            }
+            else { throw new IndexOutOfRangeException(); }
+        }
+
+        public static decimal[] TriangleMainDiagonal(decimal[,] triangleArray)
         {
             int nRows = triangleArray.GetLength(0);
             decimal[] array = new decimal[nRows];
-            for (int i = 0; i < nRows - 1; nRows++)
+            for (int i = 0; i < nRows; i++)
             {
                 array[i] = triangleArray[i, i];
             }
             return array;
         }
 
-        decimal[] ChainLadderLastDiagonal(decimal[,] triangleArray)
+        public static decimal[] ChainLadderLastDiagonal(decimal[,] triangleArray)
         {
             int nRows = triangleArray.GetLength(0);
             decimal[] array = new decimal[nRows];
             int nColumns = triangleArray.GetLength(1);
-            for (int i = 0; i < nRows - 1; nRows++)
+            for (int i = 0; i < nRows; i++)
             {
                 array[i] = triangleArray[i, nColumns-1-i];
             }
---
+++ XUnitReservingPAndCTest/UnitTest1.cs
@@ -0,0 +1,70 @@
+using Xunit;
+using ReservingPropertyAndCasualty;
+using System;
+
+namespace XUnitReservingPAndCTest
+{
+    public class UnitTest1
+    {
+        decimal[,] arr2d = new decimal[5, 5]{
+                                {1, 0, 0, 4, 3},
+                                {9, 0, 3, 3, 2},
+                                {5, 6, 0, 5, 0},
+                                {4, 3, 6, 9, 7},
+                                {3, 8, 7, 8, 2}
+                            };
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void ChainLadderGetRowNTest1()
+        {
+            decimal[] a = new decimal[5]{ 4, 3, 6, 9, 7 };
+            Assert.Equal<decimal[]>(a, ChainLadder.GetRowN(this.arr2d, 3));
+        }
+
+        [Fact]
+        public void ChainLadderGetRowNTest2()
+        {
+            decimal[] a = new decimal[5] { 1, 0, 0, 4, 3 };
+            Assert.Equal<decimal[]>(a, ChainLadder.GetRowN(this.arr2d, 0));
+        }
+
+        [Fact]
+        public void ChainLadderGetRowNTest3()
+        {
+            decimal[] a = new decimal[5] { 3, 8, 7, 8, 2 };
+            Assert.Equal<decimal[]>(a, ChainLadder.GetRowN(this.arr2d, 4));
+        }
+
+        [Fact]
+        public void ChainLadderGetRowNTest4()
+        {
+            Assert.Throws<IndexOutOfRangeException>(()=> ChainLadder.GetRowN(this.arr2d, 10));
+        }
+
+        [Fact]
+        public void ChainLadderGetColumnNTest1()
+        {
+            decimal[] a = new decimal[5] { 4, 3, 5, 9, 8 };
+            Assert.Equal<decimal[]>(a, ChainLadder.GetColumnN(this.arr2d, 3));
+        }
+
+        [Fact]
+        public void ChainLadderGetColumnNTest2()
+        {
+            decimal[] a = new decimal[5] { 1,9,5,4,3 };
+            Assert.Equal<decimal[]>(a, ChainLadder.GetColumnN(this.arr2d, 0));
+        }
+
+        [Fact]
+        public void ChainLadderGetColumnNTest3()
+        {
+            decimal[] a = new decimal[5] { 3,2,0,7,2 };
+            Assert.Equal<decimal[]>(a, ChainLadder.GetColumnN(this.arr2d, 4));
+        }
+    }
+}
