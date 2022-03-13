e4df601f5d92b37192b0c86032e4a9cfbd4e7a69
Kevin BAMOUNI <kevincedric.y@gmail.com>
2022-03-13 13:03:09 +0100

    - Chain Ladder implementation and test !!!

--- ReservingPropertyAndCasualty/HatariIChainLadder.cs
+++ ReservingPropertyAndCasualty/HatariIChainLadder.cs
@@ -8,57 +8,119 @@ namespace ReservingPropertyAndCasualty
 {
     public class HatariIChainLadder
     {
-        public HatariChainLadder()
+        public HatariIChainLadder()
         {
 
         }
 
         //Step 2
-        public decimal[,] AgeToAgeLinkRatios()
+        public static decimal[,] AgeToAgeLinkRatios(decimal[,] triangleArray)
         {
-
+            int nRows = triangleArray.GetLength(0);
+            int nColumns = triangleArray.GetLength(1);
+            decimal[,] linkRatioTriangleArray = new decimal[nRows-1, nColumns-1];
+            for (int i = 0; i < nRows-1; i++)
+            {
+                for (int j = 0; j < nColumns-1; j++)
+                {
+                    linkRatioTriangleArray[i, j] = linkRatioTriangleArray[i, j+1] / linkRatioTriangleArray[i, j];
+                }
+            }
+            return linkRatioTriangleArray;
         }
         //Step 3
-        public decimal[,] AverageAgeToAgeLinkRatios()
-        {
+        //public decimal[,] AverageAgeToAgeLinkRatios()
+        //{
 
-        }
+        //}
         // Step 6
-        public decimal[] CumulativeClaimDevelopmentLinkRatios()
+        public static decimal[] CumulativeClaimDevelopmentLinkRatios(decimal[,] triangleArray)
         {
+            int nRows = triangleArray.GetLength(0);
+            int nColumns = triangleArray.GetLength(1);
+            decimal[] linkRatio = new decimal[nColumns - 1];
+            decimal[] numerator = new decimal[nColumns-1];
+            decimal[] denominator =  new decimal[nColumns - 1];
 
+            // Numerator Calculation
+            int n = nRows - 1;
+            for (int j = 1; j < nColumns; j++)
+            {
+                numerator[j-1] = triangleArray[0, j];
+                for (int i = 1; i < n; i++)
+                {
+                    numerator[j-1] = numerator[j-1] + triangleArray[i,j];
+                }
+                n--;
+            }
+
+            // Denominator calculation
+            n = nRows - 1;
+            for (int j = 0; j < nColumns-1; j++)
+            {
+                denominator[j] = triangleArray[0, j];
+                for (int i = 1; i < n; i++)
+                {
+                    denominator[j] = denominator[j] + triangleArray[i, j];
+                }
+                n--;
+            }
+
+            // Link Ratios Calculation
+            for (int i = 0; i < nColumns - 1; i++)
+            {
+                linkRatio[i] = numerator[i] / denominator[i];
+            }
+
+            return linkRatio;
         }
+
         // 1 divide by CumulativeClaimDevelopmentLinkRatios
-        public decimal[] CumulativeReportedOrPaidPattern()
+        public decimal[] CumulativeReportedOrPaidPattern(decimal[] linkRatio)
         {
-
-        }
-        // lag of CumulativeReportingPattern :
-        // IncremetalReportingtPattern(0) = CumulativeReportingPattern(0)
-        // n > 0 : IncremetalReportingtPattern(n) = CumulativeReportingPattern(n) - CumulativeReportingPattern(n-1)
-        public decimal[] IncremetalReportedOrPaidtPattern()
-        {
-
-        }
-
-        public decimal[] ProjectionToFullTriangle()
-        {
-
+            decimal[] Pattern = linkRatio;
+            int nRows = linkRatio.GetLength(1);
+            for (int i = 0; i < nRows; i++)
+            {
+                Pattern[i] = 1 / linkRatio[i];
+            }
+            return Pattern;
         }
 
-        public decimal[] ReservePerOrigin()
+        //// lag of CumulativeReportingPattern :
+        //// IncremetalReportingtPattern(0) = CumulativeReportingPattern(0)
+        //// n > 0 : IncremetalReportingtPattern(n) = CumulativeReportingPattern(n) - CumulativeReportingPattern(n-1)
+        public decimal[] IncremetalReportedOrPaidtPattern(decimal[] cumulativeReportedOrPaidPattern)
         {
+            decimal[] Pattern = cumulativeReportedOrPaidPattern;
+            int nRows = cumulativeReportedOrPaidPattern.GetLength(1);
+            Pattern[0] = cumulativeReportedOrPaidPattern[0];
+            for (int i = 1; i < nRows; i++)
+            {
+                Pattern[i] = cumulativeReportedOrPaidPattern[i] - cumulativeReportedOrPaidPattern[i-1];
+            }
 
+            return Pattern;
         }
 
-        public decimal[] TotalReserve()
-        {
+        //public decimal[] ProjectionToFullTriangle()
+        //{
 
-        }
+        //}
 
-        public Dictionary<string,dynamic> ChainLadderFinalResults()
-        {
+        //public decimal[] ReservePerOrigin()
+        //{
 
-        }
+        //}
+
+        //public decimal[] TotalReserve()
+        //{
+
+        //}
+
+        //public Dictionary<string,dynamic> ChainLadderFinalResults()
+        //{
+
+        //}
     }
 }
--- XUnitReservingPAndCTest/UnitTest1.cs
+++ XUnitReservingPAndCTest/UnitTest1.cs
@@ -23,48 +23,48 @@ namespace XUnitReservingPAndCTest
         public void ChainLadderGetRowNTest1()
         {
             decimal[] a = new decimal[5]{ 4, 3, 6, 9, 7 };
-            Assert.Equal<decimal[]>(a, ChainLadder.GetRowN(this.arr2d, 3));
+            Assert.Equal<decimal[]>(a, GeneralArrayFuntions.GetRowN(this.arr2d, 3));
         }
 
         [Fact]
         public void ChainLadderGetRowNTest2()
         {
             decimal[] a = new decimal[5] { 1, 0, 0, 4, 3 };
-            Assert.Equal<decimal[]>(a, ChainLadder.GetRowN(this.arr2d, 0));
+            Assert.Equal<decimal[]>(a, GeneralArrayFuntions.GetRowN(this.arr2d, 0));
         }
 
         [Fact]
         public void ChainLadderGetRowNTest3()
         {
             decimal[] a = new decimal[5] { 3, 8, 7, 8, 2 };
-            Assert.Equal<decimal[]>(a, ChainLadder.GetRowN(this.arr2d, 4));
+            Assert.Equal<decimal[]>(a, GeneralArrayFuntions.GetRowN(this.arr2d, 4));
         }
 
         [Fact]
         public void ChainLadderGetRowNTest4()
         {
-            Assert.Throws<IndexOutOfRangeException>(()=> ChainLadder.GetRowN(this.arr2d, 10));
+            Assert.Throws<IndexOutOfRangeException>(()=> GeneralArrayFuntions.GetRowN(this.arr2d, 10));
         }
 
         [Fact]
         public void ChainLadderGetColumnNTest1()
         {
             decimal[] a = new decimal[5] { 4, 3, 5, 9, 8 };
-            Assert.Equal<decimal[]>(a, ChainLadder.GetColumnN(this.arr2d, 3));
+            Assert.Equal<decimal[]>(a, GeneralArrayFuntions.GetColumnN(this.arr2d, 3));
         }
 
         [Fact]
         public void ChainLadderGetColumnNTest2()
         {
             decimal[] a = new decimal[5] { 1,9,5,4,3 };
-            Assert.Equal<decimal[]>(a, ChainLadder.GetColumnN(this.arr2d, 0));
+            Assert.Equal<decimal[]>(a, GeneralArrayFuntions.GetColumnN(this.arr2d, 0));
         }
 
         [Fact]
         public void ChainLadderGetColumnNTest3()
         {
             decimal[] a = new decimal[5] { 3,2,0,7,2 };
-            Assert.Equal<decimal[]>(a, ChainLadder.GetColumnN(this.arr2d, 4));
+            Assert.Equal<decimal[]>(a, GeneralArrayFuntions.GetColumnN(this.arr2d, 4));
         }
     }
 }
